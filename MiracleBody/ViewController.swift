6be43a2e0041a86771d290cbc68e61a6e8ffcd02
Shana Dacres <shana.dacres@capgemini.com>
2017-04-11 13:19:23 +0100

    setup

---
+++ MiracleBody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiracleBody
+//
+//  Created by Shana Dacres on 11/04/2017.
+//  Copyright © 2017 Shana Dacres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
